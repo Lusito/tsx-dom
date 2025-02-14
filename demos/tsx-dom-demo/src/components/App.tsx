9d1061257227c7ca59d41ab68c45fb8db222ddab
Lusito <core@lusito.info>
2025-02-14 15:50:09 +0100

    better height for custom-elements demo

--- demos/tsx-dom-demo/src/components/App.tsx
+++ demos/tsx-dom-demo/src/components/App.tsx
@@ -7,7 +7,7 @@ export const App = () => (
         <h2>GRINCH TO DO LIST</h2>
         <TodoList />
         <h2>Custom Elements Demo</h2>
-        <div style={{ height: "800px" }}>
+        <div style={{ height: "100vh" }}>
             <p>Scroll down... you'll see a bar, which starts growing angry and changing color once in view. </p>
             <p>Don't worry. When you scroll up again, it returns to its calm state.</p>
         </div>
